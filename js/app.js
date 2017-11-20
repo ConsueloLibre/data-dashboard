72d2f02637d614daa3ec7063e3258227eb72e278
ConsueloLibre <consuelomariana.acevedo@gmail.com>
2017-11-19 21:13:44 -0300

    colores y boton

--- js/app.js
+++ js/app.js
@@ -11,17 +11,17 @@ google.load('visualization', '1.0',{'packages':['corechart']});
     	google.setOnLoadCallback(drawChart);
 
     	          function drawChart() {
-            
+
             // Create the data table.
             var data4 = new google.visualization.DataTable();
-            data4.addColumn('string', 'N° headquarters');    
-            data4.addColumn('number', 'Estudents');
+            data4.addColumn('string', 'N° headquarters');
+            data4.addColumn('number', '262 Students');
             data4.addRows([
-              ['H1', 62],
-              ['H2', 60],
-              ['H3', 80],
-              ['H4', 60],
-             
+              ['Arequipa', 62],
+              ['Mexico', 60],
+              ['Santiago', 80],
+              ['Lima', 60],
+
             ]);
             // Create the data table.
             var data5 = new google.visualization.DataTable();
@@ -29,41 +29,44 @@ google.load('visualization', '1.0',{'packages':['corechart']});
             data5.addColumn('number', 'TECH');
             data5.addColumn('number', 'HSE')
             data5.addRows([
-              ['H1', 1799, 1198],
-              ['H2', 1605, 924],
-              ['H3', 1346, 771],
-              ['H4', 901, 600],
-              
+              ['Arequipa', 1799, 1198],
+              ['Mexico', 1605, 924],
+              ['Santiago', 1346, 771],
+              ['Lima', 901, 600],
+
             ]);
 
             var data6 = new google.visualization.DataTable();
             data6.addColumn('string', 'Headquarters');
             data6.addColumn('number', 'Porcent');
-            
+
             data6.addRows([
-              ['H1', 70],
-              ['H2', 65],
-              ['H3', 90],
-              ['H4', 85],
-              
+              ['Arequipa', 70],
+              ['Mexico', 65],
+              ['Santiago', 90],
+              ['Lima', 85],
+
             ]);
 
-            
 
-            
-             var options4 = {'title':'Total Students',
+
+
+             var options4 = {'title':'Total Of Current Students',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                           'colors':['rgb(248,170,28)']};
             // Set chart options
-            var options5 = {'title':'TECH v/s HSE',
+            var options5 = {'title':'AVERAGE TECH SKILLS & HSE',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                           'colors':['rgb(255,255,45)','rgb(255,153,0)']};
             // Set chart options
             var options6 = {'title':'ASSISTANCE',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                         'colors':['rgb(248,170,28)']};
 
-            
+
 
             // Instantiate and draw our chart, passing in some options.
             var cont1 = new google.visualization.ColumnChart(document.getElementById('cont1'));
@@ -73,19 +76,19 @@ google.load('visualization', '1.0',{'packages':['corechart']});
             var cont5 = new google.visualization.BarChart(document.getElementById('cont_div5'));
             cont5.draw(data6, options6);
 
-}           
+}
   google.load('visualization', '1.0',{'packages':['corechart']});
             google.setOnLoadCallback(draw);
 
                 function draw() {
             // Create the data table.
             var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Alumnas');    
+            data.addColumn('string', 'Alumnas');
             data.addColumn('number', 'Porcentaje');
             data.addRows([
               ['AM', 80],
               ['PM', 60],
-             
+
             ]);
             // Create the data table.
             var data2 = new google.visualization.DataTable();
@@ -95,7 +98,7 @@ google.load('visualization', '1.0',{'packages':['corechart']});
             data2.addRows([
               ['AM', 1590, 1700],
               ['PM', 1600, 1500],
-              
+
             ]);
 
             var data3 = new google.visualization.DataTable();
@@ -105,21 +108,24 @@ google.load('visualization', '1.0',{'packages':['corechart']});
             data3.addRows([
               ['AM', 80, 20],
               ['PM', 85, 15],
-              
+
             ]);
 
             // Set chart options
             var options = {'title':'Aprobación del 70%',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                         'colors':['rgb(248,170,28)','rgb(255,255,95)']};
             // Set chart options
             var options2 = {'title':'Puntaje TECH v/s HSE',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                         'colors':['rgb(248,170,28)','rgb(255,255,95)']};
             // Set chart options
             var options3 = {'title':'¿LABORATORIA?',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                         'colors':['rgb(248,170,28)','rgb(255,255,95)']};
            // Instantiate and draw our chart, passing in some options.
             var cont = new google.visualization.PieChart(document.getElementById('cont'));
             cont.draw(data, options);
@@ -136,12 +142,12 @@ google.load('visualization', '1.0',{'packages':['corechart']});
                 function chart() {
             // Create the data table.
             var data7 = new google.visualization.DataTable();
-            data7.addColumn('string', 'Alumnas');    
+            data7.addColumn('string', 'Alumnas');
             data7.addColumn('number', 'Porcentaje');
             data7.addRows([
               ['AM', 80],
               ['PM', 60],
-             
+
             ]);
             // Create the data table.
             var data8 = new google.visualization.DataTable();
@@ -151,7 +157,7 @@ google.load('visualization', '1.0',{'packages':['corechart']});
             data8.addRows([
               ['AM', 1590, 1700],
               ['PM', 1600, 1500],
-              
+
             ]);
 
             var data9 = new google.visualization.DataTable();
@@ -161,21 +167,24 @@ google.load('visualization', '1.0',{'packages':['corechart']});
             data9.addRows([
               ['AM', 80, 20],
               ['PM', 85, 15],
-              
+
             ]);
 
             // Set chart options
             var options7 = {'title':'Aprobación del 70%',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                           'colors':['rgb(248,170,28)','rgb(255,255,95)']};
             // Set chart options
             var options8 = {'title':'Puntaje TECH v/s HSE',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                         'colors':['rgb(248,170,28)','rgb(255,255,95)']};
             // Set chart options
             var options9 = {'title':'¿LABORATORIA?',
                            'width':400,
-                           'height':300};
+                           'height':300,
+                         'colors':['rgb(248,170,28)','rgb(255,255,95)']};
            // Instantiate and draw our chart, passing in some options.
             var contt2 = new google.visualization.ColumnChart(document.getElementById('cont2'));
             contt2.draw(data7, options7);
@@ -183,7 +192,7 @@ google.load('visualization', '1.0',{'packages':['corechart']});
             cont7.draw(data8, options8);
             var cont8 = new google.visualization.PieChart(document.getElementById('cont_div8'));
             cont8.draw(data9, options9);
- 
+
 }
 
 
